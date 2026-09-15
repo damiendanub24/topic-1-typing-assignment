a502c3c5aa4f7f4b6e7aa1edcb8d5675c23d2c3c
362573354 <362573354@2260I-L29-007>
2026-09-15 13:02:34 -0400

    Add project files.

---
+++ topic 1 typing assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace topic_1_typing_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Damien");
+            Console.WriteLine("");
+            Console.WriteLine("Comments can be used in programming to explain what a difficult line of code does, and to leave info on known bugs, and to inform what an unfinished line of code does for later");
+            Console.WriteLine("");
+            Console.WriteLine("DDDD   K    K");
+            Console.WriteLine("D   D  K   K");
+            Console.WriteLine("D   D  K  K");
+            Console.WriteLine("D   D  K K");
+            Console.WriteLine("D   D  K  K");
+            Console.WriteLine("DDDD   K   K");
+        }
+    }
+}
